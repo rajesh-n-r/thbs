620e1cf47ec5fbd7fbdad1b9cee9930bc4d97b85
rajesh-n-r <56992379+rajesh-n-r@users.noreply.github.com>
2022-01-27 16:14:59 +0530

    Delete trainproject directory

--- trainproject/TrainTicketProjectNew/src/main/java/com/torry/MyConfig.java
+++
@@ -1,9 +0,0 @@
-package com.torry;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.torry")
-public class MyConfig {
-}
